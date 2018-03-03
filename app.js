dce40fdad00da47ae3b2d487eb510b27ef21b771
Anil Patel <ajpatel92@icloud.com>
2018-03-03 17:07:23 -0600

    firebase updates

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+
+var config = {
+    apiKey: "AIzaSyBcYCHp3iRwP2_yMoYBPsu7o1TvIv0X9KI",
+    authDomain: "pnil-world.firebaseapp.com",
+    databaseURL: "https://pnil-world.firebaseio.com",
+    projectId: "pnil-world",
+    storageBucket: "pnil-world.appspot.com",
+    messagingSenderId: "464639531572"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
 $("#sendMessage").on("click", function(event){
     event.preventDefault();
 
-    var message = $("#typeMessage").val();
-    $("#chatBox").append(message + "<br>");
-    
+    var message = $("#typeMessage").val().trim();
+
+   
+
+    database.ref().set({
+      message: message
+    });
+
     $("#typeMessage").val("");
    
 });
@@ -12,4 +31,9 @@ $(".container").keyup(function (event) {
     if (event.keyCode === 13) {
         $("#sendMessage").click();
     }
+});
+
+database.ref().on("value", function(snapshot){
+    message = snapshot.val().message;
+    $("#chatBox").append(message + "<br>");
 });
